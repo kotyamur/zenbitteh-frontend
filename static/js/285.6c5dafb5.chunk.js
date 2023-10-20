68c366a7c3ca1370506b064aad68bb4407f7e713
kotyamur <99125819+kotyamur@users.noreply.github.com>
2023-10-20 13:35:14 +0000

    Deploying to gh-pages from @ kotyamur/zenbitteh-frontend@8a23e5b67415659a70f6b46c44b13675499a09d5 🚀

--- static/js/285.6c5dafb5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[285],{285:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:"Register"})})}}}]);
-//# sourceMappingURL=285.6c5dafb5.chunk.js.map
--- static/js/38.8928c11e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[38],{38:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("div",{children:"Login"})})}}}]);
-//# sourceMappingURL=38.8928c11e.chunk.js.map
